0d164414b5f820498b153d448ffecb205063b16a
Alex Shumilov <poprygun@hotmail.com>
2021-09-03 16:50:31 -0400

    Add app accelerator config.

--- src/main/java/io/microsamples/accelerator/chachkies/Application.java
+++ src/main/java/io/microsamples/accelerator/chachkies/Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"io.microsamples.accelerator.chachkies", "io.microsamples.accelerator.chachkies.api" , "org.openapitools.configuration"})
+@ComponentScan(basePackages = {"io.microsamples.accelerator.chachkies", "io.microsamples.accelerator.chachkies.api"})
 public class Application {
 
 	public static void main(String[] args) {
