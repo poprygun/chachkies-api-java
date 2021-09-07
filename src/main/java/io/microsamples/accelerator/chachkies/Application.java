08830573b19bc330d73bdfbe5a8a99247bed9dba
Alex Shumilov <poprygun@hotmail.com>
2021-09-07 15:28:04 -0400

    Fix openapi ui links.

--- src/main/java/io/microsamples/accelerator/chachkies/Application.java
+++ src/main/java/io/microsamples/accelerator/chachkies/Application.java
@@ -5,11 +5,9 @@ import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"io.microsamples.accelerator.chachkies", "io.microsamples.accelerator.chachkies.api"})
 public class Application {
 
 	public static void main(String[] args) {
